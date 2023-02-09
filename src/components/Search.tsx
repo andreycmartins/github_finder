8a160764c9a15b4481a5bdc3b6be2aab5a062d09
andreycmartins <andreybr45@gmail.com>
2023-02-09 17:49:29 -0300

    Estilizando página inicial

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,20 +1,38 @@
-import {BsSearch} from "react-icons/bs"
+type SearchProps = {
+  loadUser: (userName: String) => Promise<void>
+}
 
-import React from "react"
+import { useState, KeyboardEvent } from "react"
+import { BsSearch } from "react-icons/bs"
 
-const Search = () => {
+import classes from "./Search.module.css"
+
+const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState("")
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName)
+    }
+  }
+
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por usuário</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
-        <input type="text" placeholder="Digite o nome do usuário"/>
-        <button>
+      <div className={classes.search_container}>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={() => loadUser(userName)}>
           <BsSearch />
         </button>
       </div>
     </div>
   )
-} 
+}
 
 export default Search
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,21 +2,36 @@ import { UserProps } from "../types/user"
 
 import React, { useState } from "react"
 import Search from "../components/Search"
+import { User } from "../components/User"
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null)
 
-  const loadUser = async (userName: string) => {
+  const loadUser = async (userName: String) => {
     const res = await fetch(`https://api.github.com/users/${userName}`)
 
     const data = await res.json()
 
-    console.log(data)
+    const { avatar_url, login, location, followers, following } = data
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    }
+
+    setUser(userData)
   }
 
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
+      {user && <p>{user.location}</p>}
+      {user && <p>{user.followers}</p>}
+      {user && <p>{user.following}</p>}
     </div>
   )
 }
